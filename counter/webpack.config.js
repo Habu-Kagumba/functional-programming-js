a9fa71e60c9bcb996f2fafa4d4a2e87f4adf99b5
Herbert Kagumba <habukagumba@gmail.com>
2019-10-05 20:27:25 +0300

    Counter Application Updates:
    - Update dependencies
    - Update port

--- counter/webpack.config.js
+++ counter/webpack.config.js
@@ -1,8 +1,10 @@
-const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js'
     },
@@ -12,21 +14,12 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['babel-preset-env'],
-                        plugins: [require('babel-plugin-transform-object-rest-spread')]
-                    }
-                }
-            }
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Simple Counter Application',
             template: 'index.html'
